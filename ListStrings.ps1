8627d96e5545ba5f07f93a8ec0dfaae5ddd4e7a1
Vasyl Zhushma <vazhushma@gmail.com>
2019-09-17 14:36:28 +0300

    Update localization script

--- ListStrings.ps1
+++ ListStrings.ps1
@@ -1,8 +1,7 @@
 ﻿# Generate locale files from the source
 
 $projName = 'XKeyBinding'
-#$localeSources = @("$projName.lua", "$($projName)_Bars.lua", "$($projName)_Config.lua", "$($projName)_Data.lua", "$($projName)_Utils.lua")
-$localeSources = @("$projName.lua", "$($projName)_Config.lua", "$($projName)_Buttons.lua")
+$localeSources = Get-ChildItem "$projName*.lua" | %{ $_.Name }
 $locales = @('enUS', 'ruRU')
 $baseLocale = 'enUS'
 $localeDir = 'Locales'
@@ -25,13 +24,15 @@ $strings = $strings | ?{ $_ -cnotmatch $ext_re }
 $locales | %{
 	$locale = $_
 	$oldFile = ".\$localeDir\locale-$locale.lua"
-	$file = "$oldFile.new"
+    $file = "$oldFile.new"
 
-	Select-String -Path $oldFile -Encoding utf8 -Pattern $old_re | %{$oldStrings = @{}} { $_.Matches | %{$oldStrings.Add($_.Groups[1].Value, $_.Groups[2].Value)} } {$oldStrings} | Out-Null
+	Select-String -Path $oldFile -Encoding utf8 -Pattern $old_re `
+			| %{$oldStrings = [hashtable]::new()} { $_.Matches | %{$oldStrings.Add($_.Groups[1].Value, $_.Groups[2].Value)} } {$oldStrings} `
+			| Out-Null
 
 	Set-Content $file ([string]::Format($headerFmt, $locale, $(if($locale -eq $baseLocale){ $trueParam } else { "" }), $total))
 
 	@($strings, $ext_strings) | %{
-		Add-Content $file ($_ | %{ if($locale -eq "enUS" -and !$oldStrings[$_]){ "L[`"$_`"] = true" } else { "L[`"{0}`"] = `"{1}`"" -f $_, $oldStrings[$_] } })
+		Add-Content $file ($_ | %{ if($locale -eq "enUS" -and !$oldStrings[$_]){ "L[`"$_`"] = true" } else { "L[`"{0}`"] = `"{1}`"" -f $_, $(if($oldStrings[$_]) { $oldStrings[$_] } else { $_ }) } })
 	}
 }
